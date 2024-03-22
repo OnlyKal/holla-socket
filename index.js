9d8f96e86e71d211b40f6d227fa362a09e25f642
Stark <onlykal64@gmail.com>
2024-03-22 11:03:29 +0100

    income message so ket

--- index.js
+++ index.js
@@ -12,22 +12,45 @@ const io = new Server(server, {
 });
 
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+
+
+io.on('connection', (socket) => {
+        //  socket.on('login',(data)=>{
+        //      console.log(data)
+        //     //  socket.on('/response-message-socketio', (req, res) => {
+        //     //     console.log('Event', req.body);
+        //     //     io.emit('response-message-socketio', req.body)
+        //     //     // res.send('Event success.');
+        //     // });
+        //  })
+        console.log("CONNECTED")
+        socket.on('response-message-socketio', (data) => {
+            console.log('Event', data);
+            io.emit('response-message-socketio',data)
+            // res.send('Event success.');
+        });
+      
 });
 
 
-app.post('/event-users', (req, res) => {
-    console.log('users', req.body);
-    io.emit('event-users', `${req.body.id}`)
-    res.send('Event success.');
-});
 
-app.post('/list-conversations-users', (req, res) => {
-    console.log('users', req.body);
-    io.emit('list-conversations-users', `${req.body.id}`)
-    res.send('Event success.');
-});
+
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+
+// app.post('/event-users', (req, res) => {
+//     console.log('users', req.body);
+//     io.emit('event-users', `${req.body.id}`)
+//     res.send('Event success.');
+// });
+
+// app.post('/list-conversations-users', (req, res) => {
+//     console.log('users', req.body);
+//     io.emit('list-conversations-users', `${req.body.id}`)
+//     res.send('Event success.');
+// });
 
 const port = 3000;
 server.listen(port, () => {
